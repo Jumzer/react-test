cc772f04632e03763f30ff58759cc5578267cf17
Jumzer <bluevictor@hotmail.fr>
2023-03-30 11:09:35 +0200

    props done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,15 @@
-function PokemonCard() {
-  const pokemon = pokemonList[1];
+function PokemonCard(props) {
+  console.log(props);
   return (
     <figure>
-      {pokemon.imgSrc !== "" ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      {props.pokemon.imgSrc !== "" ? (
+        <img src={props.pokemon.imgSrc} alt={props.pokemon.name} />
       ) : (
         <p>???</p>
       )}
-      <figcaption>{pokemon.name}</figcaption>
+      <figcaption>{props.pokemon.name}</figcaption>
     </figure>
   );
 }
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default PokemonCard;
